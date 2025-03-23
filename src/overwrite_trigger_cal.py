33d928b554e4d2b92d0611a862a12cdcaa53de8d
Konstantin Savov <konstantin.savov@notionfix.com>
2025-03-23 16:09:33 +0200

    updated files

--- src/overwrite_trigger_cal.py
+++ src/overwrite_trigger_cal.py
@@ -1,17 +1,12 @@
 # uncompyle6 version 3.9.0
 # Python bytecode version base 3.8.0 (3413)
-# Decompiled from: Python 3.8.10 (default, Nov 14 2022, 12:59:47) 
+# Decompiled from: Python 3.8.10 (default, Nov 14 2022, 12:59:47)
 # [GCC 9.4.0]
 # Embedded file name: trigger_cal.py
 import sys, os, platform
 from time import sleep
 from controller_if import ControllerInterface
 
-def kill_steam():
-	if platform.system() == 'Linux':
-		print('Stopping Steam if running...')
-		os.system('killall steam > /dev/null')
-		sleep(1.0)
 def connect_cb(hid_dev_mgr):
 	global connected
 	connected = True
@@ -40,7 +35,7 @@ else:
 	cntrlr_mgr.load_default_mappings()
 	cntrlr_mgr.load_default_settings()
 	cntrlr_mgr.trigger_cancel_cal()
-	
+
 def main():
 	try:
 		print('Reading current calibration...\n')
@@ -48,9 +43,7 @@ def main():
 		right_trigger_cal = cntrlr_mgr.trigger_get_cal(1)
 		print('Current Left Trigger Cal:  ' + str(left_trigger_cal[1:-1]))
 		print('Current Right Trigger Cal: ' + str(right_trigger_cal[1:-1]))
-		#print('Current Left STICK Cal:  ' + str(cntrlr_mgr.thumbstick_get_cal(0)))
-		#print('Current Right STICK Cal: ' + str(cntrlr_mgr.thumbstick_get_cal(1)))
-		
+
 		lmax = input("Enter LEFT  trigger MAX value (increase value to decrease start deadzone, current = " + str( left_trigger_cal[1]).rjust(5," ") +"): ")
 		lmax = int(lmax) if lmax else left_trigger_cal[1]
 		lmin = input("Enter LEFT  trigger MIN value (decrease value to decrease end deadzone,   current = " + str( left_trigger_cal[2]).rjust(5," ") +"): ")
@@ -59,21 +52,12 @@ def main():
 		rmax = int(rmax) if rmax else right_trigger_cal[1]
 		rmin = input("Enter RIGHT trigger MIN value (decrease value to decrease end deadzone,   current = " + str(right_trigger_cal[2]).rjust(5," ") +"): ")
 		rmin = int(rmin) if rmin else right_trigger_cal[2]
-		
-		print("\nPlease review changes:\n    Left Trigger Cal:  " + str(left_trigger_cal[1:-1]) + " -> " + str((lmax,lmin)) + "\n    Right Trigger Cal: " + str(right_trigger_cal[1:-1]) + " -> " + str((rmax,rmin)))
-		answer = input("\nThis may make your controls inoperable! Please double check your values! Type \"PROCEED\" to confirm: ")
-		if answer != "PROCEED":
-			return
-		
-		kill_steam()
-		
-		input("\nFinal warning! Press A button to apply.")
-		
+
 		cntrlr_mgr.trigger_set_cal(0, lmax, lmin, left_trigger_cal[3])
 		cntrlr_mgr.trigger_set_cal(1, rmax, rmin, right_trigger_cal[3])
-		
+
 		print("Done! Please go back into Gaming Mode, go to Settings -> Controller -> Test Device Inputs, and verify")
-		
+
 	except KeyboardInterrupt:
 		print('\nKeyboard Interrupt!')
 		pass
